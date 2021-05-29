6cbc303a483f89be4e3ede1604ab3a923dd26367
Aparajita-png <77184488+Aparajita-png@users.noreply.github.com>
2021-05-29 10:39:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,36 +1,44 @@
 img=""; 
 status="";
+objects=[];
 function preload(){
 img=loadImage("dog_cat.jpg")
 }
 function setup(){
-canvas=createCanvas(640,420);
+canvas=createCanvas(380,380);
 canvas.center();
+video=createCapture(VIDEO);
+video.hide();
+
 objectDetector=ml5.objectDetector('cocossd',modelLoaded);
 document.getElementById("status").innerHTML="status:detecting objects";
 }
 function modelLoaded(){
 console.log("modelLoaded");
 status=true;
-objectDetector.detect(img,gotResult);
+
 }
 function gotResult(error,results){
  if(error){
- console.error("error");
+ console.error(error);
  }
  else{
- console.log("results");
+ console.log(results);
+ objects=results;
  }
  }
- function draw(){
- image(img,0,0,640,420);
- fill("#FF0000");
- text("dog",45,75);
+ function draw(){  
+ image(video,0,0,380,380);
+ if(status!=""){
+ objectDetector.detect(video,gotResult);
+ for(i=0;i<objects.length;i++){
+ document.getElementById("status").innerHTML="status-object detector";
+ fill("#800080");
+ percent=floor(objects[i].confidence*100);
+ text(objects[i].label+""+percent+"%",objects[i].x,objects[i].y);
  noFill();
- stroke("#FF0000");
- rect(30,60,450,350);
- fill("#FF0000");
- text("cat",320,120);
- noFill();
- rect(300,90,270,320);
+ stroke("#800080");
+ rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+ }
+ }
 }
